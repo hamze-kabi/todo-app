354b39d6e94a5d6f16340438f1bf323d49ce6801
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-04 18:38:28 +0330

    add tailwind, edit app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,26 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex justify-center items-center h-screen w-screen">
+    <div id="aa" class="w-40 h-40 bg-green-500"></div>
+  </div>
 </template>
 
 <style scoped></style>
+<!-- 
+day night toggle
+farsi eng
+complete/delete
+hide complete
+sort based on time or alphabet
+count of active and complete tasks
+created by
+
+suggested components:
+│   ├── Header.vue
+│   ├── TodoInput.vue
+│   ├── TodoList.vue
+│   ├── TodoItem.vue
+│   ├── TodoFilters.vue
+│   └── Footer.vue
+-->
