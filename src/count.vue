633cbe5d7779deb52d4c739271c4144bab2415a0
hamze-kabi <104631365+HamzeKabi@users.noreply.github.com>
2025-09-10 18:15:57 +0330

    Update count.vue

--- src/count.vue
+++ src/count.vue
@@ -2,7 +2,7 @@
   <p>{count}</p>
   <button @click="count.double">double</button>
   <button @click="count.increment">increment</button>
-  <button @click="count.reset">reset</button>
+  <button @click="count.reset">resets</button>
 </template>
 
 <script setup>
